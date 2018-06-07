a4205efd2c98ca6248513a1cbfc1f621f592fdf4
Tyress <tyress@synon.co.jp>
2018-06-07 15:47:26 +0900

    Preventing SwiftyJSON compile fail with Swift 4.1

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", .upToNextMajor(from: "16.0.0"))
+        .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", .upToNextMajor(from: "17.0.1"))
     ],
     targets: [
         .target(name: "CodeGenerator", dependencies: ["AWSSDKSwiftCore", "SwiftyJSON"]),
